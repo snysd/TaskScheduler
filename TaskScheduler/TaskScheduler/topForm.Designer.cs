846b5cc8331ba6756e841d88146fd3b7abbc6ca1
DESKTOP-1VP9R0T\sny2s <sny2sd@gmail.com>
2021-11-25 21:46:18 +0900

    トップ画面追加

---
+++ TaskScheduler/TaskScheduler/topForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace TaskScheduler
+{
+    partial class topForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listViewTask = new System.Windows.Forms.ListView();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("BIZ UDPゴシック", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Task List";
+            // 
+            // listViewTask
+            // 
+            this.listViewTask.HideSelection = false;
+            this.listViewTask.Location = new System.Drawing.Point(46, 123);
+            this.listViewTask.Name = "listViewTask";
+            this.listViewTask.Size = new System.Drawing.Size(1080, 542);
+            this.listViewTask.TabIndex = 1;
+            this.listViewTask.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(44, 691);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(146, 29);
+            this.buttonRemove.TabIndex = 2;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(244, 691);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(162, 28);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(478, 691);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(158, 28);
+            this.buttonEdit.TabIndex = 4;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // topForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 749);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.listViewTask);
+            this.Controls.Add(this.label1);
+            this.Name = "topForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewTask;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonEdit;
+    }
+}
+
--- TaskScheduler/TaskScheduler/topForm.cs
+++ TaskScheduler/TaskScheduler/topForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TaskScheduler
 {
-    public partial class Form1 : Form
+    public partial class topForm : Form
     {
-        public Form1()
+        public topForm()
         {
             InitializeComponent();
         }
